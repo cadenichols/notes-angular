ffe433464927d70d993054e2f8dc6f6cfffe4197
cnichols <mellowdious@mail.com>
2015-03-03 10:35:16 -0800

    initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+var plugins = require('./config/plugins');
+var routes = require('./config/routes');
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL);
+server.connection({port:process.env.PORT});
+
+mongoose.connection.once('open', function() {
+  server.register(plugins, function() {
+    server.route(routes);
+    server.start(function() {
+      console.log('info', server.info.uri);
+      console.log('info', process.env.MONGO_URL);
+    });
+  });
+});
